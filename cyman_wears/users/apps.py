85cd6c223ebd809ed6328b4011ff300bdc27d353
Manu-722 <Emmanuelmulongo46@gmail.com>
2025-08-09 13:18:36 +0300

    updated apps

--- cyman_wears/users/apps.py
+++ cyman_wears/users/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
-from . import signals  
+  
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cyman_wears.users'
 
     def ready(self):
-        import users.signals  
+        # import users.signals  
+         from . import signals
